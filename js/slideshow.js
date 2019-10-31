26439cd66214e5ddba517d3176b23004addc636f
Elton Dan <roxas190@gmail.com>
2019-10-30 22:12:27 -0700

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,6 +3,7 @@ var slideIndex = [1,1,1,1,1,1,1];
 var slideId = ["mySlides1", "mySlides2","mySlides3", "mySlides4","mySlides5", "mySlides6", "mySlides7"]
 var dotId = ["dot1", "dot2","dot3", "dot4","dot5", "dot6", "dot7"]
 
+var i;
 for (i=0; i<slideIndex.length;i++){
   showSlides(1, i);
 }
