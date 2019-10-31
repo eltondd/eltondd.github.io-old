56c2d901f9020de3264602e1947abb8c22dfdd61
Elton Dan <roxas190@gmail.com>
2019-10-30 21:19:52 -0700

    carousel update

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,28 +1,33 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = [1,1,1,1,1,1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["mySlides1", "mySlides2","mySlides3", "mySlides4","mySlides5", "mySlides6", "mySlides7"]
+var dotId = ["dot1", "dot2","dot3", "dot4","dot5", "dot6", "dot7"]
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+for (i=0; i<slideIndex.length;i++){
+  showSlides(1, i);
+}
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n);
 }
 
-function showSlides(n) {
+function showSlides(n, no) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName(slideId[no]);
+  var dots = document.getElementsByClassName(dotId[no]);
+  if (n > slides.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex[no]-1].className += " active";
 }
